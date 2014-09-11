29215c6ffe90794fd6ed65a95d8ccce1e4b91440
bags307 <noreply>
2014-09-11 20:48:36 +0000

    Add module bar

---
+++ bar.js
@@ -0,0 +1,16 @@
+/**
+ * @module //bar
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Bar
+ * @extends Montage
+ */
+exports.Bar = Montage.specialize(/** @lends Bar# */ {
+    constructor: {
+        value: function Bar() {
+            this.super();
+        }
+    }
+});
